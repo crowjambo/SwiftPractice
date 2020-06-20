10555030fdfad529d3d61b1705b27fc2da1596cf
crowjambo <evaldascrow@gmail.com>
2020-06-20 14:55:20 +0300

    Initial Commit

---
+++ SwiftPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPractice
+//
+//  Created by Evaldas Paulauskas on 2020-06-20.
+//  Copyright © 2020 Evaldas Paulauskas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
